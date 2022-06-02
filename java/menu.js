d0a82f044719b113edb6158b6acd40a321bec6a0
Julianmacri <julian.macri12@gmail.com>
2022-06-02 01:41:00 -0300

    archivo subido

---
+++ java/menu.js
@@ -0,0 +1,31 @@
+function calcular(operacion)
+{
+
+ 
+    var categoriavalor=(document.getElementById('categoriavalor').value);
+    var cantidad=parseInt(document.getElementById('cantidad').value);
+    
+    switch(operacion)
+    {
+        case 'resumen':
+            if(categoriavalor == "estudiante"){
+                document.getElementById('result').value="Total a pagar: $" + ((200*0,80)*cantidad);
+                break;
+            }
+            if(categoriavalor == "trainee"){
+                document.getElementById('result').value="Total a pagar: $" + ((200*0,50)*cantidad);
+                break;
+            }
+           if(categoriavalor == "junior"){
+                document.getElementById('result').value="Total a pagar: $" + ((200*0,15)*cantidad);
+                break;
+            }
+        case 'borrar':
+            window.location.reload();
+
+           
+        
+       
+    }
+}
+
